6d15c0404f74ebdfa720ded610fe8cf14de79e09
Mauro Ue <mauro.ue@datacake.com.br>
2024-09-14 10:22:15 -0300

    added some templates for login

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -65,7 +65,9 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -136,3 +138,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+login_redirect_url = "/"
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic.base import TemplateView
 
 from backend import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.register, name="register"),
-    path('login/', views.login, name="login")
-
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('dashboard/user/', TemplateView.as_view(template_name='home.html'), name="home"),
 ]
