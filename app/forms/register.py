83ccbfad8168e813d45939fc102e76f359125964
Mauro Ue <mauro.ue@datacake.com.br>
2024-09-26 21:21:16 -0300

    fixed dashboard

--- app/forms/register.py
+++ app/forms/register.py
@@ -4,10 +4,12 @@ from app.models.users import User
 
 
 class UserRegistrationForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text='Required')
-    name = forms.CharField(max_length=100, required=True, help_text='Required')
-    dob = forms.DateField(required=True, widget=forms.SelectDateWidget(years=range(1900, 2025)))
+    email = forms.EmailField(max_length=200, help_text="Required")
+    name = forms.CharField(max_length=100, required=True, help_text="Required")
+    dob = forms.DateField(
+        required=True, widget=forms.SelectDateWidget(years=range(1900, 2025))
+    )
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', 'name', 'dob')
+        fields = ("username", "email", "password1", "password2", "name", "dob")
